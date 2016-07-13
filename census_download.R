510f735d8933bf66fa228f72d2187a8f1c6eb4d1
Gavin Sutton <gdsutton@outlook.com>
2016-07-13 09:30:32 +0100

    Added merge

--- census_download.R
+++ census_download.R
@@ -1,7 +1,15 @@
-#######
-wd <- "C:/Users/gdsut/Documents/Data/Census"
-setwd(wd)
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Census Data Download ---------------------------------------------------------
 
+# Gavin Sutton
+# 20160712
+
+census_wd <- getwd()
+census_wd 
+setwd(census_wd)
+
+library(stringr)
 
 table_list <- read.csv("census_table_download.csv")
 str(table_list)
@@ -21,13 +29,11 @@ for (i in tables_to_download) {
   download.file(file_url,saved_name)
 }
 
-
 file_info <- file.info(dir(wd,pattern = ".zip"))
 
 # some didnt download, try those again
 retry_tables <- tables_to_download %in% substr(rownames(file_info[file_info$size > 100000,]),1,7)
 retry_tables <- tables_to_download[!retry_tables]
-
 
 for (i in retry_tables) {
   file_url <- paste("http://www.nomisweb.co.uk/output/census/2011/",i,"_2011_oa.zip",sep = "")
@@ -55,19 +61,23 @@ for (i in folders_to_check) {
   }
 }
 
-
 #gather file names and read data into list
 data_files <- list.files(paste(wd,"/data_extracts",sep = ""))
 
 dat <- list()
 
-for (i in data_files) {
-  dat[[i]] <- read.csv(paste(wd,"/data_extracts/",i,sep = ""))
+for (i in tables_to_download) {
+  data_in  <- read.csv(paste(wd,"/data_extracts/",toupper(i),"DATA.CSV",sep = ""))
+  
+  col_name_begin <- table_list$Column_Begin[table_list[1] == toupper(i)]
+  cols_to_take <- as.numeric(str_sub(names(data_in),-2)) >= col_name_begin
+  cols_to_take[is.na(cols_to_take)] <- TRUE
+  
+  dat[[i]] <- data_in[,c(cols_to_take)]
+  
 }
 
 # merge list elements into single data frame
 complete <- Reduce(function(x,y){merge(x,y,all=TRUE)},dat)
 
-rm(dat)
-
-head(complete)
+write.csv(complete,"Census_KS_OA_Combined.csv")
